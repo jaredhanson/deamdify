70e0548052e52c71939d5191f92129c489931e57
airportyh <airportyh@gmail.com>
2013-11-02 00:32:03 -0400

    Added the semicolons that I forgot.

--- index.js
+++ index.js
@@ -30,57 +30,55 @@ module.exports = function (file) {
 
   function end() {
     
-    var seemsToSupportsCommonJS = false
-    var commonJSWrapper = false
+    var seemsToSupportsCommonJS = false;
+    var commonJSWrapper = false;
 
     var output = falafel(data, {raw: true}, function(node){
       if (node.type === 'ReturnStatement'){
-        var fun = parentFunction(node)
+        var fun = parentFunction(node);
         if (isDefineCall(fun.parent)){
-          node.update('module.exports = ' + node.argument.source())
+          node.update('module.exports = ' + node.argument.source());
         }
       }
       if (isCommonJSRequire(node)){
-        seemsToSupportsCommonJS = true
+        seemsToSupportsCommonJS = true;
       }
       if (isUMDCheck(node)){
         // get rid of the if statement entirely and pull
         // the consequent block up
         var src = node.consequent.body.map(function(exp){
-          return exp.source()
-        }).join('\n')
-        node.update(src)
+          return exp.source();
+        }).join('\n');
+        node.update(src);
       }
       if (isDefineCall(node)){
 
         var args = node.arguments.slice(0)
-        var moduleDef
-        var deps = []
-        var render = renderModuleLiteral
+          , moduleDef
+          , deps = []
+          , render = renderModuleLiteral;
 
-        if (isString(args[0])) args.shift()
+        if (isString(args[0])) args.shift();
         if (isArray(args[0])){
-
-          deps = getDeps(args.shift())
+          deps = getDeps(args.shift());
         }
         if (args.length > 0){
-          moduleDef = args.shift()
+          moduleDef = args.shift();
           if (isFunction(moduleDef)){
-            render = renderModuleFactory
+            render = renderModuleFactory;
           }else if (isIdentifier(moduleDef)){
-            render = renderModuleIdentifier
+            render = renderModuleIdentifier;
           }
         }
 
         if (isCommonJSWrapper(deps, moduleDef)){
           commonJSWrapper = true;
         }
-        node.update(render(deps, moduleDef))
-
+        node.update(render(deps, moduleDef));
       }
     });
 
-    data = (seemsToSupportsCommonJS && !commonJSWrapper) ? data : output + ''
+    data = (seemsToSupportsCommonJS && !commonJSWrapper) ? data : output + '';
     
     stream.queue(data);
     stream.queue(null);
@@ -89,19 +87,19 @@ module.exports = function (file) {
 };
 
 function renderModuleFactory(deps, factory){
-  var defVars = getFuncParams(factory)
-  var reqs = requiresSection(deps, defVars)
-  var body = functionBody(factory)
-  return reqs + body
+  var defVars = getFuncParams(factory);
+  var reqs = requiresSection(deps, defVars);
+  var body = functionBody(factory);
+  return reqs + body;
 }
 
 function renderModuleLiteral(deps, literal){
-  var reqs = requiresSection(deps, [])
-  return reqs + 'module.exports = ' + literal.source()
+  var reqs = requiresSection(deps, []);
+  return reqs + 'module.exports = ' + literal.source();
 }
 
 function requiresSection(deps, depVars){
-  var vars = zip(deps, depVars)
+  var vars = zip(deps, depVars);
   var ret = vars
     .filter(function(vr){
       var dep = vr[0]
@@ -115,9 +113,9 @@ function requiresSection(deps, depVars){
       }else{
         return 'var ' + depVar
       }
-    }).join('\n')
-  if (ret.length > 0) ret += '\n'
-  return ret
+    }).join('\n');
+  if (ret.length > 0) ret += '\n';
+  return ret;
 }
 
 function renderModuleIdentifier(deps, identifier){
@@ -130,24 +128,24 @@ function renderModuleIdentifier(deps, identifier){
       '}else{\n' +
       '  module.exports = ' + identifier.name + ';\n' +
       '}\n' +
-    '}());'
+    '}());';
 }
 
 function isArray(node){
-  return node.type === 'ArrayExpression'
+  return node.type === 'ArrayExpression';
 }
 
 function isString(node){
   return node.type === 'Literal' &&
-      typeof eval(node.raw) === 'string'
+      typeof eval(node.raw) === 'string';
 }
 
 function isFunction(node){
-  return node.type === 'FunctionExpression'
+  return node.type === 'FunctionExpression';
 }
 
 function isIdentifier(node){
-  return node.type === 'Identifier'
+  return node.type === 'Identifier';
 }
 
 function isDefineCall(node){
@@ -158,27 +156,27 @@ function isDefineCall(node){
 function functionBody(funcNode){
   return funcNode.body.body.map(function(b){
     return b.source();
-  }).join('\n')
+  }).join('\n');
 }
 
 function parentFunction(node){
-  var next = node.parent
+  var next = node.parent;
   while(next && next.type !== 'FunctionExpression'){
-    next = next.parent
+    next = next.parent;
   }
-  return next
+  return next;
 }
 
 function getDeps(arg){
   return arg.elements.map(function(s){
-    return s.value
-  })
+    return s.value;
+  });
 }
 
 function getFuncParams(func){
   return func.params.map(function(p){
-    return p.name
-  })
+    return p.name;
+  });
 }
 
 function isCommonJSRequire(node){
@@ -186,43 +184,43 @@ function isCommonJSRequire(node){
     node.callee.name === 'require' &&
     node.arguments.length === 1 &&
     node.arguments[0].type === 'Literal' &&
-    typeof eval(node.arguments[0].raw) === 'string'
+    typeof eval(node.arguments[0].raw) === 'string';
 }
 
 function isCommonJSWrapper(deps, factory){
-  if (!factory || factory.type !== 'FunctionExpression') return false
-  var params = factory.params
+  if (!factory || factory.type !== 'FunctionExpression') return false;
+  var params = factory.params;
   return factory && params.length >= 3 &&
     params[0].name === 'require' &&
     params[1].name === 'exports' &&
-    params[2].name === 'module'
+    params[2].name === 'module';
 }
 
 function isUMDCheck(node){
   return node.type === 'IfStatement' &&
     node.test.type === 'LogicalExpression' &&
     isTypeCheck(node.test.left) &&
-    isAmdPropertyCheck(node.test.right)
+    isAmdPropertyCheck(node.test.right);
 
   function isTypeCheck(node){
     return node.type === 'BinaryExpression' &&
       (node.operator === '===' || node.operator === '==') &&
       isTypeof(node.left) &&
       node.right.type === 'Literal' &&
-      node.right.value === 'function'
+      node.right.value === 'function';
   }
 
   function isTypeof(node){
     return node.type === 'UnaryExpression' &&
       node.operator === 'typeof' &&
       node.argument.type === 'Identifier' &&
-      node.argument.name === 'define'
+      node.argument.name === 'define';
   }
 
   function isAmdPropertyCheck(node){
     return node.type === 'MemberExpression' &&
       node.object.name === 'define' &&
-      node.property.name === 'amd'
+      node.property.name === 'amd';
   }
 }
 
