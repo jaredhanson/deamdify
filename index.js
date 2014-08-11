83e46eb480731e3ae1bd0fd56dfea701d301e2a4
Nathan Rajlich <nathan@tootallnate.net>
2014-08-11 13:26:40 -0700

    index: remove trailing whitespace

--- index.js
+++ index.js
@@ -26,28 +26,28 @@ var through = require('through')
  */
 module.exports = function (file) {
   var data = '';
-  
+
   var stream = through(write, end);
   return stream;
-  
+
   function write(buf) { data += buf }
   function end() {
     var ast = esprima.parse(data)
       , tast
       , isAMD = false;
-    
+
     //console.log('-- ORIGINAL AST --');
     //console.log(util.inspect(ast, false, null));
     //console.log('------------------');
-    
+
     // TODO: Ensure that define is a free variable.
     // TODO: Implement support for amdWeb UMD modules.
-    
+
     estraverse.replace(ast, {
       enter: function(node) {
         if (isDefine(node)) {
           var parents = this.parents();
-          
+
           // Check that this module is an AMD module, as evidenced by invoking
           // `define` at the top-level.  Any CommonJS or UMD modules are pass
           // through unmodified.
@@ -60,7 +60,7 @@ module.exports = function (file) {
         if (isDefine(node)) {
           if (node.arguments.length == 1 && node.arguments[0].type == 'FunctionExpression') {
             var factory = node.arguments[0];
-            
+
             if (factory.params.length == 0) {
               tast = createProgram(factory.body.body);
               this.break();
@@ -72,13 +72,13 @@ module.exports = function (file) {
           } else if (node.arguments.length == 1 && node.arguments[0].type == 'ObjectExpression') {
             // object literal
             var obj = node.arguments[0];
-            
+
             tast = createModuleExport(obj);
             this.break();
           } else if (node.arguments.length == 2 && node.arguments[0].type == 'ArrayExpression' && node.arguments[1].type == 'FunctionExpression') {
             var dependencies = node.arguments[0]
               , factory = node.arguments[1];
-            
+
             var ids = dependencies.elements.map(function(el) { return el.value });
             var vars = factory.params.map(function(el) { return el.name });
             var reqs = createRequires(ids, vars);
@@ -91,7 +91,7 @@ module.exports = function (file) {
           } else if (node.arguments.length == 3 && node.arguments[0].type == 'Literal' && node.arguments[1].type == 'ArrayExpression' && node.arguments[2].type == 'FunctionExpression') {
             var dependencies = node.arguments[1]
               , factory = node.arguments[2];
-            
+
             var ids = dependencies.elements.map(function(el) { return el.value });
             var vars = factory.params.map(function(el) { return el.name });
             var reqs = createRequires(ids, vars);
@@ -104,26 +104,26 @@ module.exports = function (file) {
           }
         } else if (isReturn(node)) {
           var parents = this.parents();
-          
+
           if (parents.length == 5 && isDefine(parents[2]) && isAMD) {
             return createModuleExport(node.argument);
           }
         }
       }
     });
-    
+
     if (!isAMD) {
       stream.queue(data);
       stream.queue(null);
       return;
     }
-    
+
     tast = tast || ast;
-    
+
     //console.log('-- TRANSFORMED AST --');
     //console.log(util.inspect(tast, false, null));
     //console.log('---------------------');
-    
+
     var out = escodegen.generate(tast);
     stream.queue(out);
     stream.queue(null);
@@ -151,20 +151,20 @@ function createProgram(body) {
 
 function createRequires(ids, vars) {
   var decls = [];
-  
+
   for (var i = 0, len = ids.length; i < len; ++i) {
     if (['require', 'module', 'exports'].indexOf(ids[i]) != -1) { continue; }
-    
+
     decls.push({ type: 'VariableDeclarator',
       id: { type: 'Identifier', name: vars[i] },
-      init: 
+      init:
         { type: 'CallExpression',
           callee: { type: 'Identifier', name: 'require' },
           arguments: [ { type: 'Literal', value: ids[i] } ] } });
   }
-  
+
   if (decls.length == 0) { return null; }
-  
+
   return { type: 'VariableDeclaration',
     declarations: decls,
     kind: 'var' };
@@ -172,10 +172,10 @@ function createRequires(ids, vars) {
 
 function createModuleExport(obj) {
   return { type: 'ExpressionStatement',
-    expression: 
+    expression:
      { type: 'AssignmentExpression',
        operator: '=',
-       left: 
+       left:
         { type: 'MemberExpression',
           computed: false,
           object: { type: 'Identifier', name: 'module' },
